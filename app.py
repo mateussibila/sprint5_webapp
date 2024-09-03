5d895aeef6a969f564ce1d72c9e0db65aff78a78
mateussibila <mateussibila@gmail.com>
2024-09-03 16:37:30 +1000

    reorganized and  documented

--- app.py
+++ app.py
@@ -2,49 +2,48 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
+# reading the dataset
 car_data = pd.read_csv('vehicles.csv')
 
+# Section 1 - Data view
+st.header('Data viewer')  # Showing header
+# Building a filter
 model_counts = car_data['model'].value_counts()
 models_limit = 500
 models_to_keep = model_counts[model_counts > models_limit].index
 car_data_filtered = car_data[car_data['model'].isin(models_to_keep)]
-
-st.header('Data viewer')
-filter_dataset = st.checkbox(f'Include models with less than {models_limit} ads')
-
-
-if filter_dataset:
-    st.write(car_data_filtered)    
+# Checkbox to turn on and off the filter
+filter_num_ads = st.checkbox(
+    f'Exclude models with less than {models_limit} ads')
+# Defining what to show based on the checkbox state
+if filter_num_ads:
+    st.write(car_data_filtered)  # if on, show filtered dataset
 else:
-    st.write(car_data)
+    st.write(car_data)  # if off, show unfiltered
 
-st.header('Vehicle types by manufacturer')
-st.write("INSERIR gráfico de barras interativo")
-
-st.header('Histogram of condition vs model_year')
-hist_button = st.button('Create histogram')  # criar um botão
-
-if hist_button:  # se o botão for clicado
-    # escrever uma mensagem
-    st.write('Creating a histogram for the car sale adds dataset')
-
-    # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # exibir um gráfico Plotly interativo
+# Section 2 - Histogram
+st.header('Histogram of `model_year`')  # Showing header
+# Showing a button to create an histogram
+hist_button = st.button('Create histogram')
+# If the button is clicked
+if hist_button:
+    # Write a message
+    st.write('Creating a histogram by `model_year`')
+    # Create a histogram by 'model_year'
+    fig = px.histogram(car_data, x="model_year")
+    # Show the histogram
     st.plotly_chart(fig, use_container_width=True)
 
-
-st.header('Compare price distribution between manufacturers')
-scatter_plot_button = st.button('Creat scatter plot')
-
+# Section 3 - Scatter plot - price and odometer
+# showing a header
+st.header('Compare `price` distribution per `odometer` mesurements')
+# Showing a button to create the plot
+scatter_plot_button = st.button('Create scatter plot')
+# If the button is clicked
 if scatter_plot_button:
-    # escrever uma mensagem
-    st.write('Creating a scatter plot for the car sale adds dataset')
-
+    # Write a message
+    st.write('Creating a scatter plot')
+    # Create a scatter plot of price distribution per odometer mesurements
     fig = px.scatter(car_data, x='odometer', y='price')
-
+    # Show the scatter plot
     st.plotly_chart(fig, use_container_width=True)
-
-# select box
-# https://docs.streamlit.io/develop/api-reference/widgets/st.selectbox
